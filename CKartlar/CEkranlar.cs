2c0a5b25b7d9e47efe1d1691b2e1b0762c31a957
kozannur <kozannnur@gmail.com>
2017-01-10 18:12:49 +0300

    FrmLoginFrmKullanici
    Kullanici tablosu ve adı gecen formların tasarımını yaptım.

---
+++ CKartlar/CEkranlar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makrosoft.Classlar
+{
+    class Ekranlar
+    {
+        public void StokAnaGrupTanitimKartiAc(Boolean Sec)
+        {
+            Makrosoft.StokModulu.frmStokAnaGrupTanitimKarti frm = new StokModulu.frmStokAnaGrupTanitimKarti();
+            if(Sec==true)
+            {
+                //frm.Secimicinacildimi = true;
+                frm.ShowDialog();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ CKartlar/CKullanicilar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makrosoft.CKartlar
+{
+    class CKullanicilar
+    {
+        public int Kull_KayNo { get; set; }
+        public Boolean Kilitli { get; set; }
+        public Int16 Kull_Ekleyen { get; set; }
+        public DateTime Kull_EklenenTarih { get; set; }
+        public Int16 Kull_Degistiren { get; set; }
+        public DateTime Kull_DegistirilenTarih { get; set; }
+        public string Kull_OzelAlan1 { get; set; }
+        public string Kull_OzelAlan2 { get; set; }
+        public string Kull_OzelAlan3 { get; set; }
+        public int Kull_No { get; set; }
+        public string Kull_Adi { get; set; }
+        public string Kull_Sifre { get; set; }
+        public string Kull_UzunAdi { get; set; }
+        public string Kull_GirisAdi { get; set; }
+        public string Kull_EMail { get; set; }
+        public Boolean Kull_InSystem { get; set; }
+        public Byte Kull_ProgNo { get; set; }
+        public int Kull_Modules { get; set; }
+        public int Kull_Random { get; set; }
+        public String Kull_DBase { get; set; }
+        public Byte Kull_Buffer { get; set; }
+        public Byte Kull_SifreTipi { get; set; }
+        public DateTime Kull_SifreDegisim_Tarihi { get; set; }
+        public Boolean Kull_Pasif { get; set; }
+        public string Kull_IlkIP { get; set; }
+        public string Kull_SonIP { get; set; }
+
+    }
+}
